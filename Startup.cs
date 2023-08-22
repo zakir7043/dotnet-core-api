8417b7c663959db39d1019530b653d7308287d57
ShanthiTV <113419029+ShanthiTV@users.noreply.github.com>
2023-08-23 01:06:21 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -25,12 +25,27 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri("https://myKV9898.vault.azure.net/"), new DefaultAzureCredential(),options);
+            
+            KeyVaultSecret secret = client.GetSecret("AppSecret");
+            
+            string secretValue = secret.Value;
             services.AddControllers();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = secretValue });
             });
 
             services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
@@ -64,7 +79,7 @@ namespace TodoApi
 
             app.UseAuthorization();
 
-            SecretClientOptions options = new SecretClientOptions()
+            /*SecretClientOptions options = new SecretClientOptions()
             {
                 Retry =
                 {
@@ -78,7 +93,7 @@ namespace TodoApi
             
             KeyVaultSecret secret = client.GetSecret("AppSecret");
             
-            string secretValue = secret.Value;
+            string secretValue = secret.Value;*/
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
