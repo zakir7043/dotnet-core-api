bbeee7be2068fba0b698305c0fd49b83ed3b3ca6
zakir7043 <159785509+zakir7043@users.noreply.github.com>
2024-03-22 00:01:34 -0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-          /*  SecretClientOptions options = new SecretClientOptions()
+            SecretClientOptions options = new SecretClientOptions()
             {
                 Retry =
                 {
@@ -35,18 +35,18 @@ namespace TodoApi
                     Mode = RetryMode.Exponential
                 }
             };
-            var client = new SecretClient(new Uri("https://mykeyvault89.vault.azure.net/"), new DefaultAzureCredential(),options);
+            var client = new SecretClient(new Uri("https://mykeyvault7043.vault.azure.net/"), new DefaultAzureCredential(),options);
             
             KeyVaultSecret secret = client.GetSecret("AppSecret");
             
-            string secretValue = secret.Value;*/
+            string secretValue = secret.Value;
             services.AddControllers();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = secretValue });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = secretValue });
             });
 
             services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
